f661d0516c4d70687de137b3830612f9699a1548
Trang Doan <doantrangltt@gmail.com>
2025-11-05 16:18:28 +0700

    First commit - Upload Express project

---
+++ app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var productsRouter = require('./routes/products');
+var storeRouter = require('./routes/store');
+var contactRouter = require('./routes/contact');
+var featureRouter = require('./routes/feature');
+var blogRouter = require('./routes/blog');
+var testimonialsRouter = require('./routes/testimonial');
+var errorRouter = require('./routes/error');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/products',productsRouter);
+app.use('/store',storeRouter);
+app.use('/contact',contactRouter);
+app.use('/feature', featureRouter);
+app.use('/blog',blogRouter);
+app.use('/testimonial',testimonialsRouter);
+app.use('/error', errorRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
